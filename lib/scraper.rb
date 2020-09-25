ee51cf4041edee8c76eede17fb80891315288b67
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 23:07:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,31 +1,17 @@
 class Scraper
-  attr_accessor :file, :parsed_file, :all_pokemon, :db
 
   def initialize(db)
-    self.db = db
-    self.file = File.open("pokemon_index.html")
-    self.parsed_file = Nokogiri::HTML.parse(file)
-    self.all_pokemon = catch_em_all
-  end
-
-  def catch_em_all
-    self.all_pokemon = parsed_file.css(".infocard-tall")
-  end
-
-  def get_pokemon_name_from(node)
-    node.css(".ent-name").text
-  end
-
-  def get_pokemon_type_from(node)
-    node.css(".itype").text
+    @db = db
+    #@html = File.open("pokemon_index.html")
   end
 
   def scrape
-    all_pokemon.each do |pk_node|
-      pk_name = get_pokemon_name_from(pk_node)
-      pk_type = get_pokemon_type_from(pk_node)
-      Pokemon.save(pk_name, pk_type, db)
+    doc = Nokogiri::HTML(open("pokemon_index.html"))
+
+    doc.search("span.infocard-tall").collect do |pokemon|
+      name = pokemon.search("a.ent-name").text
+      type = pokemon.search("small.aside a").text
+      Pokemon.save(name, type, @db)
     end
   end
-
 end
