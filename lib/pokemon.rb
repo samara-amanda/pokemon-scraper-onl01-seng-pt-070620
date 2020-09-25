7622825e6c2dde9750a3912cb51fae9d99cfd33c
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 23:08:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,11 @@
 class Pokemon
   attr_accessor :name, :type, :id, :db, :hp
 
-  def initialize (id, name, type, db)
+  def initialize (id, name, type, hp, db)
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
   end
 
